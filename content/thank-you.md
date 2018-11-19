f283eafd383a0ea3545f17d37b55dd838f062af0
Zach Russell <zach@protechig.com>
2018-11-19 16:17:11 -0500

    add contact form

---
+++ content/thank-you.md
@@ -0,0 +1,8 @@
+---
+title: 'Thank you for Reaching Out!'
+date: 2018-09-29T16:39:00-04:00
+seo_title: Expert WordPress Developer in Philadelphia | Zach Russell
+description: Are you looking to hire a WordPress expert in Philadelphia? I have over 8 years experience building WordPress sites for small businesses and Fortune 500 companies alike. Hire me today!
+---
+
+I'll be in touch within the next business day. In the meantime, why not [follow me on Twitter](https://twitter.com/zacharyRRussell)?
--- themes/zachrussell/static/js/global.js
+++ themes/zachrussell/static/js/global.js
@@ -1,4 +1,42 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.sidenav');
-  var instances = M.Sidenav.init(elems);
-});
+document.addEventListener( 'DOMContentLoaded', function() {
+  var elems = document.querySelectorAll( '.sidenav' );
+  var instances = M.Sidenav.init( elems );
+} );
+
+var form = document.getElementById( 'contactForm' );
+if ( form ) {
+  form.addEventListener( 'submit', function( event ) {
+    event.preventDefault();
+
+    sendData();
+  } );
+}
+
+function sendData() {
+  var formBody = {};
+  var FD = new FormData( form );
+  let parameters = [ ...FD.entries() ] // expand the elements from the .entries() iterator into an actual array
+    .map( e => encodeURIComponent( e[0] ) + '=' + encodeURIComponent( e[1] ) ); // transform the elements into encoded key-value-pairs
+  var encodedData = parameters.join( '&' );
+  console.log( encodedData );
+
+  fetch(
+    'https://us-central1-zachs-personal-automation.cloudfunctions.net/contactForm',
+    {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      mode: 'no-cors', // no-cors, cors, *same-origin
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+
+        //'Content-Type': 'application/json'
+      },
+      body: encodedData // body data type must match "Content-Type" header
+    }
+  ).then( data => {
+    if ( data ) {
+      redirect: window.location.replace( '/thank-you' );
+    } else {
+      alert( 'ERROR' );
+    }
+  } );
+}
